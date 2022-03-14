b3f4fb5b8993110026ca4417dec3528b3af02481
Matreshkarp <94852791+Matreshkarp@users.noreply.github.com>
2022-03-14 21:19:28 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# Слито в https://t.me/HACKER_PHONE_VIP
+
+API_TOKEN = '5214085945:AAGSNcEqG8wYqYZo5D_EOOE6I4z6LpgwsAs' # Токен бота! Брать с @BotFather
+admin = 1655468174 # ID Админа, тобишь твой! Брать с @getmyid_bot
+
+# Слито в https://t.me/HACKER_PHONE_VIP
